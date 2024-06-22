4ea8a427cb1ef51473d03bc88302352a5786f80f
Richard Ojok <richieee@live.co.uk>
2024-06-22 13:57:20 +0100

    modified:   convex/_generated/api.d.ts 	new file:   convex/auth.config.ts 	new file:   convex/clerk.ts 	new file:   convex/http.ts 	deleted:    convex/products.ts 	modified:   convex/schema.ts 	deleted:    convex/tasks.ts 	new file:   convex/users.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/home/left-panel.tsx 	new file:   src/middleware.ts 	modified:   src/providers/convex-client-provider.tsx

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+    providers: [
+        {
+        domain: "https://deep-cow-25.clerk.accounts.dev",
+        applicationID: "convex,"
+    },
+    ]
+};
+
+export default authConfig
